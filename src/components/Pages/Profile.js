d96c286ce1598bd4a8f182b65fb624512ca1f171
unknown <kstreme@hotmail.com>
2023-11-15 17:20:25 +0100

    Add profile photo and save it in firebase

--- src/components/Pages/Profile.js
+++ src/components/Pages/Profile.js
@@ -4,28 +4,25 @@ import { Link } from "react-router-dom";
 import { motion } from "framer-motion";
 import "./profile.css";
 import { useCookies } from "react-cookie";
-import jwtDecode from "jwt-decode";
-import { firestore } from "../../services/Firebase/firebase";
-import { doc, updateDoc } from 'firebase/firestore';
+import jwtDecode from 'jwt-decode';
+import { firestore } from '../../services/Firebase/firebase';
+import { doc, getDoc, updateDoc } from 'firebase/firestore';
+import { getStorage, ref, uploadBytes, getDownloadURL } from 'firebase/storage';
 
 const Profile = () => {
   const [email, setEmail] = useState("");
   const [name, setName] = useState("");
   const [description, setDescription] = useState("");
-
+  const [photoURL, setPhotoURL] = useState("");
   const [cookies] = useCookies(["userToken"]);
 
   useEffect(() => {
     if (cookies.userToken) {
-      try {
-        const userData = parseUserDataFromToken(cookies.userToken);
-        if (userData) {
-          setEmail(userData.email);
-          setName(userData.name);
-          // Optionally, fetch and set the existing description from Firestore
-        }
-      } catch (error) {
-        console.log("Error decoding token:", error);
+      const userData = parseUserDataFromToken(cookies.userToken);
+      if (userData) {
+        setEmail(userData.email);
+        setName(userData.name);
+        fetchUserData(userData.email);
       }
     }
   }, [cookies]);
@@ -41,27 +38,56 @@ const Profile = () => {
     return null;
   };
 
-  const handleEmailChange = (e) => {
-    setEmail(e.target.value);
-  };
+  const fetchUserData = async (userEmail) => {
+    try {
+      const docRef = doc(firestore, "users", userEmail);
+      const docSnap = await getDoc(docRef);
 
-  const handleNameChange = (e) => {
-    setName(e.target.value);
-  };
-
-  const handleDescriptionChange = (e) => {
-    setDescription(e.target.value);
+      if (docSnap.exists()) {
+        setDescription(docSnap.data().description);
+        setPhotoURL(docSnap.data().photoURL || ""); // Set photo URL if available
+      } else {
+        console.log("No such document!");
+      }
+    } catch (error) {
+      console.error("Error fetching user data:", error);
+    }
   };
 
   const updateDescriptionInFirestore = async () => {
     try {
-      const userDocRef = doc(firestore, 'users', email); // Correct way to get a document reference
+      const userDocRef = doc(firestore, 'users', email);
       await updateDoc(userDocRef, { description: description });
       alert('Description updated successfully!');
     } catch (error) {
       console.error('Error updating description: ', error);
       alert('Error updating description: ' + error.message);
     }
+  };
+
+  const handleImageUpload = async (event) => {
+    const file = event.target.files[0];
+    if (!file) return;
+
+    const storage = getStorage();
+    const storageRef = ref(storage, 'profilePictures/' + email);
+
+    try {
+      const snapshot = await uploadBytes(storageRef, file);
+      const newPhotoURL = await getDownloadURL(snapshot.ref);
+      setPhotoURL(newPhotoURL);
+
+      const userDocRef = doc(firestore, 'users', email);
+      await updateDoc(userDocRef, { photoURL: newPhotoURL });
+      alert('Profile picture updated successfully!');
+    } catch (error) {
+      console.error('Error uploading profile picture: ', error);
+      alert('Error uploading profile picture: ' + error.message);
+    }
+  };
+
+  const handleDescriptionChange = (e) => {
+    setDescription(e.target.value);
   };
 
   return (
@@ -75,13 +101,19 @@ const Profile = () => {
         <div className="profile-header">
           <h1 className="profile-title">Profile</h1>
         </div>
+        {photoURL && <img src={photoURL} alt="Profile" className="profile-picture" />}
         <Form className="profile-form">
+          <Form.Group controlId="formProfilePicture">
+            <Form.Label>Profile Picture</Form.Label>
+            <Form.Control type="file" onChange={handleImageUpload} />
+          </Form.Group>
+
           <Form.Group controlId="formEmail">
             <Form.Label>Email Address</Form.Label>
             <Form.Control
               type="email"
               value={email}
-              onChange={handleEmailChange}
+              readOnly
             />
           </Form.Group>
 
@@ -90,7 +122,7 @@ const Profile = () => {
             <Form.Control
               type="text"
               value={name}
-              onChange={handleNameChange}
+              readOnly
             />
           </Form.Group>
 
@@ -104,12 +136,7 @@ const Profile = () => {
             />
           </Form.Group>
 
-          {/* Save Button */}
-          <Button
-            type="button"
-            variant="primary"
-            onClick={updateDescriptionInFirestore}
-          >
+          <Button variant="primary" onClick={updateDescriptionInFirestore}>
             Save Description
           </Button>
         </Form>
